449f2b32f323f1c67bf0be1a1613c4292ec187fb
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-10 17:44:34 +0200

    removed iframe tag

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,11 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <!-- <iframe src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/"></iframe>
         <object data="https://testReactApprouter.cfapps.us10.hana.ondemand.com/"
         width="400"
         height="400"
         type="text/html"> 
-        </object>  -->
+        </object> 
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/" width="400" height="400"></embed>
       </main>
       <Footer />
