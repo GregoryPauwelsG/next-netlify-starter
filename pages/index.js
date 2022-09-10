0a5ca00cdfc8a30d707ac033190c622c724039bf
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-10 14:01:44 +0200

    Change in title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Customers Webiste" />
+        <Header title="Customer's Webiste" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
