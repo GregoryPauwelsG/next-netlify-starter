50ca0fcba27892799fa1cc8954b5eb2a00deffd7
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-12 16:59:09 +0200

    a tag

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,11 @@ export default function Home() {
            if (typeof(mylink) == 'string') href=mylink; else href=mylink.href; window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); return false; } 
       </SCRIPT>
 
-Read more: https://html.com/javascript/popup-windows/#ixzz7egZoE3NC
       <main>
         <Header title="Customer's Webiste" />
         <p></p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE" width="400" height="400"></embed>
-        <A HREF="/code-samples/basicpopup/" onClick="return popup("https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE", 'notes')">my popup</A>
+        <A HREF="#" onClick="return popup("https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE", 'notes')">my popup</A>
       </main>
       <Footer />
     </div>
