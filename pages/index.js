abfb929b083af11b693193ad9eaf267b83a9934d
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-12 16:40:55 +0200

    changed a tag onclick

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         <Header title="Customer's Webiste" />
         <p></p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE" width="400" height="400"></embed>
-        <a href="" onclick="callWebPage(); return false;">Open page</a>
+        <a href="" onclick="callWebPage; return false;">Open page</a>
       </main>
       <Footer />
     </div>
