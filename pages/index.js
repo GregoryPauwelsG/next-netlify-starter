4fba3fe9a2ae929c4f044be5f6e745bf0b0e36aa
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-12 11:51:25 +0200

    changed a tag

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Customer's Webiste" />
         <p></p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/" width="400" height="400"></embed>
-        <a href="https://testreactapprouter.cfapps.us10.hana.ondemand.com/" target="popup" onclick="window.open('https://testreactapprouter.cfapps.us10.hana.ondemand.com/','',' scrollbars=yes,menubar=no,width=500, resizable=yes,toolbar=no,location=no,status=no')">link to React app</a>
+        <a href="https://testreactapprouter.cfapps.us10.hana.ondemand.com/" target="popup" onclick="window.open('https://testreactapprouter.cfapps.us10.hana.ondemand.com/','name',width=600,height=400)">link to React app</a>
       </main>
       <Footer />
     </div>
