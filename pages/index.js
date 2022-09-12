717c7d6d0920de79c2926f2c68efd9c8702296d3
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-12 16:57:19 +0200

    a tag changed

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,18 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <script>
-        function callWebPage(){
-                window.open("https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE");
-        }
-      </script>
+      <SCRIPT TYPE="text/javascript"> 
+        function popup(mylink, windowname) 
+          { if (! window.focus)return true; var href; 
+           if (typeof(mylink) == 'string') href=mylink; else href=mylink.href; window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); return false; } 
+      </SCRIPT>
+
+Read more: https://html.com/javascript/popup-windows/#ixzz7egZoE3NC
       <main>
         <Header title="Customer's Webiste" />
         <p></p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE" width="400" height="400"></embed>
-        <a href="#" onclick="callWebPage(); return false;">Open page</a>
+        <A HREF="/code-samples/basicpopup/" onClick="return popup("https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE", 'notes')">my popup</A>
       </main>
       <Footer />
     </div>
