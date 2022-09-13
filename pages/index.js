db9ed82e3a249c693f1e2c4cd1470069f2657084
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-13 08:53:02 +0200

    romving a tag

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,10 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SCRIPT TYPE="text/javascript"> 
-        function popup(mylink, windowname) 
-          { if (! window.focus)return true; var href; 
-           if (typeof(mylink) == 'string') href=mylink; else href=mylink.href; window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); return false; } 
-      </SCRIPT>
-
       <main>
         <Header title="Customer's Webiste" />
         <p></p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE" width="400" height="400"></embed>
-        <A HREF="#" onClick="return popup("https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/00163E063FDC1ED487FD43C39DFCEBBE", 'notes')">my popup</A>
       </main>
       <Footer />
     </div>
