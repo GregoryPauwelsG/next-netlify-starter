7b83ff847719999a7fe620c39c8376737b99de2d
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-13 11:23:39 +0200

    adding info

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
       </Head>
       <main>
         <Header title="Customer's Webiste 1" />
-        <p></p>
+        <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
+        <p>React app is loaded on this page showing objects with a Category equal to 'Servicemedewerker' </p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker" width="700" height="400"></embed>
       </main> 
       <Footer />
