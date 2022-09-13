dc27c88cbd3196e492c33ee084d9021de4e9666d
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-13 17:07:52 +0200

    changed p tag

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
       <main>
         <Header title="Customer's Webiste 1" />
-        <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
+        <p>Website deployed to Netlify to mock the behaviour of a real website. Netlify provides a development platform for web applications and dynamic websites </p>
         <p>React app is loaded on this page showing the first 5 objects with a Category equal to 'Servicemedewerker' </p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker" width="700" height="400"></embed>
       </main> 
