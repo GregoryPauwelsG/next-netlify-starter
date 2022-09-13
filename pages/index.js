88922ccba68f7d4187df33f00f33a8f332aba2af
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-13 11:35:46 +0200

    changed a tag

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
         <p>React app is loaded on this page showing objects with a Category equal to 'Servicemedewerker' </p>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker" width="700" height="400"></embed>
-        <a href="JavaScript:newPopup('https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker');">Open a popup window</a>
+        <a href="javascript:newPopup('https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker');">Open a popup window</a>
 
       </main> 
       <Footer />
