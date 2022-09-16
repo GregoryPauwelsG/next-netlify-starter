c470f055f4817f20e6a19e6dcb169cb3fc1af367
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-16 15:51:47 +0200

    adding iframe

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ export default function Home() {
         <Header title="Customer's Webiste 1" />
         <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
         <p>React app is loaded on this page showing the first 5 objects with a Category equal to 'Servicemedewerker' </p>
+        <iframe src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker" title="W3Schools Free Online Web Tutorials"></iframe>
         <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/Servicemedewerker" width="700" height="400"></embed>
+
       </main> 
       <Footer />
     </div>
