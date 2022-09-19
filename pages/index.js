ab977cfa10a1aad94f8655def140bcf89394f49b
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-19 13:27:03 +0200

    changed width

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <Header title="Customer's Webiste 1" />
         <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
         <p>React app is loaded on this page showing the first 5 objects with a Category equal to 'Servicemedewerker' </p>
-        <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/radiant-licorice-0bc8bd.netlify.app\_next\static\css\ac0345a1409f63c5.css/Servicemedewerker" width="700" height="400"></embed>
+        <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/radiant-licorice-0bc8bd.netlify.app\_next\static\css\ac0345a1409f63c5.css/Servicemedewerker" width="800" height="400"></embed>
 
       </main> 
       <Footer />
