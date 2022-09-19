869ca24bbbc29fd1ff8e1f86320d81f577797ba8
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-09-19 11:58:53 +0200

    changed soruce

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <Header title="Customer's Webiste 1" />
         <p>Website deployed to Netlify. Netlify provides a development platform for web applications and dynamic websites </p>
         <p>React app is loaded on this page showing the first 5 objects with a Category equal to 'Servicemedewerker' </p>
-        <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/radiant-licorice-0bc8bd.netlify.app/_next/static/css/921ec2eac5cd9779.css/Servicemedewerker" width="700" height="400"></embed>
+        <embed type="text/html" src="https://testreactapprouter.cfapps.us10.hana.ondemand.com/#/tickets/63283b68196eec007cbeaa7e--radiant-licorice-0bc8bd.netlify.app\_next\static\css\a5f0a14eb27b4cb7.css/Servicemedewerker" width="700" height="400"></embed>
 
       </main> 
       <Footer />
